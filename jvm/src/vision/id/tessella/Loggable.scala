cda9300b690c84c2569ff565d2df37a7ae5dcc9e
mario <mario.callisto@gmail.com>
2019-01-31 08:08:12 +0100

    Loggable trait

---
+++ jvm/src/vision/id/tessella/Loggable.scala
@@ -0,0 +1,16 @@
+package vision.id.tessella
+
+import ch.qos.logback.classic.Level
+import org.slf4j.LoggerFactory
+
+trait Loggable {
+
+  def setLogLevel(level: Level): Unit =
+    LoggerFactory
+      .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+      .asInstanceOf[ch.qos.logback.classic.Logger]
+      .setLevel(level)
+
+  val WARN: Level = Level.WARN
+
+}
--- jvm/test/src/vision/id/tessella/preSubtractTest.scala
+++ jvm/test/src/vision/id/tessella/preSubtractTest.scala
@@ -1,33 +1,23 @@
 package vision.id.tessella
 
-import ch.qos.logback.classic.Level
-import org.slf4j.LoggerFactory
+import scala.util.Try
 
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
+
 import vision.id.tessella.Tessella.Tiling
 
-import scala.util.Try
+class preSubtractTest extends FlatSpec with AddUtils with Loggable {
 
-class preSubtractTest extends FlatSpec with AddUtils {
-
-  def setLogLevel(level: Level): Unit =
-    LoggerFactory
-      .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
-      .asInstanceOf[ch.qos.logback.classic.Logger]
-      .setLevel(level)
-
-  setLogLevel(Level.WARN)
+  setLogLevel(WARN)
 
   // ---------------- subtracting single node ----------------
 
   "Subtracting a 6-degree node" must "be valid" in {
-    setLogLevel(Level.WARN)
     assert((Tiling.fromVertex(Full.s("(3*6)")) -= 1).edges.toString === "EdgeSet(5-6, 2-3, 6-7, 3-4, 7-2, 4-5)")
   }
 
   "Subtracting the only node not on the perimeter" can "be valid" in {
-    setLogLevel(Level.WARN)
     assert(
       (Tiling.fromVertex(Full.s("(3*3.4*2)")) -= 1).edges.toString === "EdgeSet(5-6, 2-3, 6-7, 3-4, 7-8, 4-5, 8-2)")
   }
@@ -35,7 +25,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
   def octagonFull: Tiling = Tiling.fromVertex(Full.s("(4.8*2)"))
 
   it can "also be NOT valid" in {
-    setLogLevel(Level.WARN)
     assertThrows[IllegalArgumentException](octagonFull -= 1)
   }
 
@@ -44,7 +33,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
       "non removable non perimeter node 1"
 
   "Subtracting a perimeter node adjacent to another 2-degree node" must "NOT be valid" in {
-    setLogLevel(Level.WARN)
     assertThrows[IllegalArgumentException](octagonFull -= 2)
   }
 
@@ -55,7 +43,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
   def fourSquares: Tiling = Tiling.fromVertex(Full.s("(4*4)"))
 
   "Subtracting a perimeter node adjacent only to > 2-degree nodes" can "be valid" in {
-    setLogLevel(Level.WARN)
     assert((fourSquares -= 9).edges.toString === "EdgeSet(1-2, 5-6, 2-3, 6-7, 6=1, 3-4, 7-8, 4=1, 4-5, 8-1)")
   }
 
@@ -66,7 +53,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
   // ---------------- subtracting single edge ----------------
 
   "Subtracting an edge whose endpoints are not on the perimeter" must "NOT be valid" in {
-    setLogLevel(Level.WARN)
     assertThrows[IllegalArgumentException](octagonFull -= Side(1, 2))
   }
 
@@ -75,17 +61,14 @@ class preSubtractTest extends FlatSpec with AddUtils {
       "endpoints of single edge 1~2 must be both on perimeter"
 
   "Subtracting an non perimeter edge whose endpoints are on the perimeter" can "be valid" in {
-    setLogLevel(Level.WARN)
     assert((Tiling.fromVertex(Vertex.s("(3*2)")) -= Side(1, 3)) === Square.toTiling)
   }
 
   it can "also be NOT valid" in {
-    setLogLevel(Level.WARN)
     assertThrows[IllegalArgumentException](fourSquares -= Side(1, 2))
   }
 
   "Subtracting a perimeter edge with a 2-degree node" must "NOT be valid" in {
-    setLogLevel(Level.WARN)
     assertThrows[IllegalArgumentException](fourSquares -= Side(2, 3))
   }
 
@@ -94,7 +77,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
       "perimeter edge 2~3 has node of degree 2"
 
   "Subtracting a perimeter edge with > 2-degree nodes" must "be valid" in {
-    setLogLevel(Level.WARN)
     assert(
       (Tiling.fromVertex(Full.s("(3*3.4*2)")) -= Side(3, 4)).edges.toString === "EdgeSet(1=2, 5-6, 5=1, 2-3, 6-7, 3-1, 7=1, 7-8, 4-1, 4-5, 8-2)")
   }
@@ -102,24 +84,20 @@ class preSubtractTest extends FlatSpec with AddUtils {
   // ---------------- subtracting multiple edges / nodes ----------------
 
   "Subtracting all nodes" must "be valid" in {
-    setLogLevel(Level.WARN)
     assert((Square.toTiling --= Set(1, 2, 3, 4)).edges.toString === "EdgeSet()")
   }
 
   "Subtracting perimeter nodes not forming a single path" can "be valid" in {
-    setLogLevel(Level.WARN)
     val t = Tiling.squareNet(3, 2)
     assert(
       (t --= Set(1, 4)).edges.toString === "EdgeSet(9-10, 5-9, 5-6, 2-3, 2-6, 6=7, 6=10, 3-7, 10-11, 7=11, 7-8, 11-12, 8-12)")
   }
 
   it can "also be NOT valid" in {
-    setLogLevel(Level.WARN)
     assertThrows[IllegalArgumentException](fourSquares --= Set(3, 7))
   }
 
   "Subtracting perimeter 2-degree nodes forming a single path adjacent to another 2-degree node" must "NOT be valid" in {
-    setLogLevel(Level.WARN)
     assertThrows[IllegalArgumentException](octagonFull --= Set(5, 6))
   }
 
@@ -128,26 +106,22 @@ class preSubtractTest extends FlatSpec with AddUtils {
       "perimeter nodes form a path adjacent to perimeter node of degree 2"
 
   "Subtracting perimeter 2-degree nodes forming a single path not adjacent only to > 2-degree nodes" must "be valid" in {
-    setLogLevel(Level.WARN)
     assert(
       (octagonFull --= Set(5, 6, 7, 8, 9)).edges.toString === "EdgeSet(15-2, 1=2, 2-3, 3-4, 10-11, 10-1, 4-1, 11-12, 12-13, 13-14, 14-15)")
   }
 
   "Subtracting non perimeter nodes" can "be valid" in {
-    setLogLevel(Level.WARN)
     assert(
       (Tiling.squareNet(3, 2) --= Set(6, 7)).edges.toString === "EdgeSet(9-10, 1-2, 1-5, 5-9, 2-3, 3-4, 10-11, 4-8, 11-12, 8-12)")
   }
 
   it can "also be NOT valid" in {
-    setLogLevel(Level.WARN)
     assertThrows[IllegalArgumentException](Tiling.squareNet(4, 2) --= Set(7, 8))
   }
 
   // ---------------- miscellaneous ----------------
 
   "Success of '-=' subtract" must "modify the mutable Tiling" in {
-    setLogLevel(Level.WARN)
     val t             = Tiling.squareNet(2, 2)
     val originalEdges = t.edges.toString
     val originalSize  = t.edges.length
@@ -157,7 +131,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
   }
 
   "Failure of '-=' subtract" must "anyway modify the mutable Tiling" in {
-    setLogLevel(Level.WARN)
     val t             = Tiling.squareNet(2, 2) - 9
     val originalEdges = t.edges.toString
     val originalSize  = t.edges.length
@@ -167,7 +140,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
   }
 
   "Success of '-' subtract" must "NOT modify the mutable Tiling" in {
-    setLogLevel(Level.WARN)
     val t             = Tiling.squareNet(2, 2)
     val originalEdges = t.edges.toString
     assert(Try(t - 9).isSuccess)
@@ -175,7 +147,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
   }
 
   "Failure of '-' subtract" must "NOT modify the mutable Tiling" in {
-    setLogLevel(Level.WARN)
     val t             = Tiling.squareNet(2, 2) - 9
     val originalEdges = t.edges.toString
     assert(Try(t - 1).isFailure)
@@ -183,18 +154,15 @@ class preSubtractTest extends FlatSpec with AddUtils {
   }
 
   "Success of '--=' subtract" must "modify the mutable Tiling" in {
-    setLogLevel(Level.WARN)
     val t             = Tiling.squareNet(2, 2)
     val originalEdges = t.edges.toString
     val originalSize  = t.edges.length
     assert(Try(t --= Set(9)).isSuccess)
     assert(t.edges.length < originalSize)
     assert(t.edges.toString !== originalEdges)
-
   }
 
   "Failure of '--=' subtract" must "modify the mutable Tiling" in {
-    setLogLevel(Level.WARN)
     val t             = Tiling.squareNet(2, 2) - 9
     val originalEdges = t.edges.toString
     val originalSize  = t.edges.length
@@ -204,7 +172,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
   }
 
   "Success of '--' subtract" must "NOT modify the mutable Tiling" in {
-    setLogLevel(Level.WARN)
     val t             = Tiling.squareNet(2, 2)
     val originalEdges = t.edges.toString
     val originalSize  = t.edges.length
@@ -214,7 +181,6 @@ class preSubtractTest extends FlatSpec with AddUtils {
   }
 
   "Failure of '--' subtract" must "NOT modify the mutable Tiling" in {
-    setLogLevel(Level.WARN)
     val t             = Tiling.squareNet(2, 2) - 9
     val originalEdges = t.edges.toString
     val originalSize  = t.edges.length
